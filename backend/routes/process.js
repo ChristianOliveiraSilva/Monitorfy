e906c1344beb77e64e3eefa420570961809203d5
ChristianOliveiraSilva <gamesparamundo123@gmail.com>
2022-02-23 19:01:14 -0400

    adding process route

--- backend/routes/process.js
+++ backend/routes/process.js
@@ -1,4 +1,23 @@
+const os = require('os');
 
 module.exports = function(req, res) {
-    
+    console.log(os.arch())
+    console.log(os.cpus())
+    console.log(os.endianness())
+    console.log(os.freemem())
+    console.log(os.getPriority())
+    console.log(os.homedir())
+    console.log(os.hostname())
+    console.log(os.loadavg())
+    console.log(os.platform())
+    console.log(os.release())
+    console.log(os.tmpdir())
+    console.log(os.totalmem())
+    console.log(os.type())
+    console.log(os.uptime())
+    console.log(os.version())
+    console.log(os.userInfo())
+    console.log(os.networkInterfaces())
+
+    res.send('See console')
 };
